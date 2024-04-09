78430f12ebecd20023281c74fe5005566149c8a4
solomonraza <solomonrazak99@gmsil.com>
2024-04-09 16:01:28 -0700

    state management initialized

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Home from './components/Home';
 import AddEmployer from './components/AddEmployer';
 import DisplayEmployer from './components/DisplayEmployer';
 import Nav from './Nav';
+import { DataContext } from './DataContext';
+
+const [formData, setFormData] = useState({
+  name: "",
+  email: "",
+  position: "",
+  department: "",
+  startDate: "",
+});
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <DataContext.Provider value={{formData, setFormData}}>
     <div className=""> 
       <Nav />
       <Routes>
@@ -19,6 +30,7 @@ function App() {
         <Route path="/displayemployer" element={<DisplayEmployer />}/>
       </Routes>
     </div>
+    </DataContext.Provider>
   )
 }
 
